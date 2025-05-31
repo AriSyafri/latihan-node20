bceb2b21252987337bb14fcdc77adeb498f04727
AriSyafri <diamondThink15@gmail.com>
2025-05-31 13:48:59 +0700

    membersihkan app dari komentar

--- 15-express-view-engine/app.js
+++ 15-express-view-engine/app.js
@@ -4,15 +4,6 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    // hanya teks
-    //   res.send('Halaman home!');
-
-    // menggunakan json
-    // res.json({
-    //     nama: 'ari',
-    //     email: 'ari@gmail.com',
-    //     noHp: '0848484848',
-    // });
 
     // menggunakan sendfile
     res.sendFile('./index.html', {root: __dirname });
@@ -43,54 +34,3 @@ app.listen(port, () => {
 });
 
 
-
-
-// const http = require('http');
-// const fs = require('fs');
-// const port = 1234;
-
-// const renderHTML = (path, res) => {
-//     fs.readFile(path, (err, data) => {
-//         if(err) {
-//             res.writeHead(404);
-//             res.write('Error : file not found');
-//         } else {
-//             res.write(data);
-//         }
-//         res.end();
-//     });  
-// };
-
-// http
-//     .createServer((req, res) => {
-        
-//         res.writeHead(200, {
-//             'Content-Type': 'text/html',
-//         });
-        
-//         const url = req.url;
-//         // if (url === '/about') {
-//         //     renderHTML('./about.html', res)
-            
-//         // } else if(url === '/contact') {
-//         //     renderHTML('./contact.html', res)       
-//         // } else {
-//         //     renderHTML('./index.html', res)
-//         // }
-
-//         switch(url) {
-//             case '/about':
-//                 renderHTML('./about.html',res);
-//                 break;
-//             case '/contact':
-//                 renderHTML('./contact.html',res);
-//                 break;
-//             default:
-//                 renderHTML('./index.html',res);
-//                 break;
-//         }
-
-//     })
-//     .listen(port, () => {
-//         console.log(`Server is listening on port ${port}`);
-//     });
