09d94ed3b7b84e0ce48f1259a1b19110c74b24a7
AriSyafri <diamondThink15@gmail.com>
2025-06-01 22:21:32 +0700

    hapus halaman yang tidak digunakan

--- coba2.js
+++
@@ -1,5 +0,0 @@
-function cetakNama(nama) {
-    return `halo, nama saya ${nama}`;
-}
-
-module.exports = cetakNama;
--- index.js
+++
@@ -1,5 +0,0 @@
-const cetakNama = require ('./coba2');
-
-// const cetakNama = (nama) => `hi, nama saya ${nama}`;
-
-console.log(cetakNama('Ari Syafri'));
