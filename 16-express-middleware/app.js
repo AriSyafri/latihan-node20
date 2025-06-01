75202aca7e72a136aff824ee9c361411c991e9be
AriSyafri <diamondThink15@gmail.com>
2025-06-01 22:15:34 +0700

    menambah morgan sebagai third party middleware

--- 16-express-middleware/app.js
+++ 16-express-middleware/app.js
@@ -1,13 +1,16 @@
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
 // gunakan ejs
 app.set('view engine', 'ejs');
 
+// third party middleware
 app.use(expressLayouts);
+app.use(morgan('dev'));
 
 // Built-in middleware
 app.use(express.static('public')); 
