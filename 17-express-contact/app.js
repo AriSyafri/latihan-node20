d8b28cf82b1bbbc9c0075d997df573741e4e5a06
AriSyafri <diamondThink15@gmail.com>
2025-06-03 15:34:45 +0700

    menambah alert input data error

--- 17-express-contact/app.js
+++ 17-express-contact/app.js
@@ -75,10 +75,16 @@ app.post('/contact', [
 ], (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()});
+        // return res.status(400).json({errors: errors.array()});
+        res.render('add-contact', {
+            title: 'Form tambah data contact',
+            layout: 'layouts/main-layout',
+            errors: errors.array(),
+        });
+    } else {
+        addContact(req.body);
+        res.redirect('/contact');
     }
-    // addContact(req.body);
-    // res.redirect('/contact');
 });
 
 
