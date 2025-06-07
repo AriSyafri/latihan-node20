c1d42288c093978ca162291df77fc7ca06ccf894
AriSyafri <diamondThink15@gmail.com>
2025-06-07 22:24:55 +0700

    menambah fitur hapus contact

--- 23-mongo-contact/app.js
+++ 23-mongo-contact/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
 const { body, validationResult, check } = require('express-validator');
+const methodOverride = require('method-override');
 
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -13,6 +14,10 @@ const Contact = require('./model/contact');
 
 const app = express();
 const port = 3000;
+
+// setup method override
+app.use(methodOverride('_method'));
+
 
 
 // gunakan ejs
@@ -125,7 +130,29 @@ app.post('/contact', [
     }
 });
 
+// proses delet contact
+// app.get('/contact/delete/:nama', async (req, res) => {
+//     const contact = await Contact.findOne({nama: req.params.nama});
 
+//     // jika kontact tidak ada
+//     if (!contact) {
+//         res.status(404);
+//         res.send('<h1>404</h1>');
+//     } else {
+//         Contact.deleteOne({_id: contact.id}).then((result) => {
+//             req.flash('msg', 'Data contact berhasil dihapus');
+//             res.redirect('/contact');
+//         });
+//     }
+// });
+
+app.delete('/contact', (req, res) => {
+    // res.send(req.body);
+    Contact.deleteOne({nama: req.body.nama }).then((result) => {
+        req.flash('msg', 'Data contact berhasil dihapus');
+        res.redirect('/contact');
+    });
+});
 
 
 // halaman detail contact
